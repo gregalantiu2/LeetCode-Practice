ac0c1286bb7f9c76fb60bebf00070272bccb4b29
Greg Tiu <tiugreg2@gmail.com>
2024-05-17 22:35:58 -0400

    wip

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,11 +1,20 @@
 namespace LeetCode_Practice
 {
+    public partial class Solution
+    {
+        public int Reverse(int x)
+        {
+            return 1;
+        }
+    }
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-            Assert.Equal(1, 1);
+            Solution solution = new Solution();
+
+            Assert.Equal(1, solution.Reverse(1));
         }
     }
 }
