a7d0c06bbc2c451d7802f87dbc36903c9340f19c
Greg Tiu <tiugreg2@gmail.com>
2024-05-21 20:44:08 -0400

    Refactor RemoveDuplicatesfromSortedArray

--- Problems/RemoveDuplicatesfromSortedArray.cs
+++ Problems/RemoveDuplicatesfromSortedArray.cs
@@ -6,14 +6,15 @@ namespace LeetCode_Practice
                 return 0;
             }
     
-            int j = 0;
+            int j = 1;
             for (int i = 1; i < nums.Length; i++) {
-                if (nums[j] != nums[i]) {
-                    nums[++j] = nums[i];
+                if (nums[i - 1] != nums[i]) {
+                    nums[j] = nums[i];
+                    j++;
                 }
             }
     
-            return j + 1;
+            return j;
         }
     }
     public class RemoveDuplicatesfromSortedArray
