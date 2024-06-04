39decdffb0901aecd4face38d26127e3efd8149a
Greg Tiu <tiugreg2@gmail.com>
2024-06-04 12:32:13 -0400

    ImplementingStackusingQueues solved

---
+++ Problems/ImplementStackusingQueues.cs
@@ -0,0 +1,50 @@
+namespace LeetCode_Practice
+{
+    public class ImplementStackusingQueues {
+        Queue<int> queue;
+    
+        public MyStack() {
+            queue = new Queue<int>();
+        }
+        
+        public void Push(int x) {
+            queue.Enqueue(x);
+        }
+        
+        public int Pop() {
+            for(int i = 0; i < queue.Count - 1; i++)
+            {   
+                queue.Enqueue(queue.Dequeue());
+            }
+    
+            return queue.Dequeue();
+        }
+        
+        public int Top() {
+            return new Queue<int>(queue.Reverse()).Peek();
+        }
+        
+        public bool Empty() {
+            return queue.Count == 0;
+        }
+    }
+    
+    /**
+     * Your MyStack object will be instantiated and called as such:
+     * MyStack obj = new MyStack();
+     * obj.Push(x);
+     * int param_2 = obj.Pop();
+     * int param_3 = obj.Top();
+     * bool param_4 = obj.Empty();
+    **/
+
+    public class ImplementStackusingQueues
+    {
+        [Fact]
+        public void ImplementStackusingQueues_Case1()
+        {
+            Solution solution = new Solution();
+            //Assert.Equal(X,solution.Test(x));
+        }
+    }
+}
