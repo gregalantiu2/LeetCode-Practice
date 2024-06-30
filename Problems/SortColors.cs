52a9e1dabfa87f6742d720eb0709bff402f7d310
Greg Tiu <tiugreg2@gmail.com>
2024-06-29 22:55:12 -0400

    SortColors solved

--- Problems/SortColors.cs
+++ Problems/SortColors.cs
@@ -1,37 +1,38 @@
 namespace LeetCode_Practice
 {
-                for(int n = 0; n < counts[m]; n++)
-                //Loop each each frequency
-            for(int j = 0; j < counts.Length; j++)
-            {
-    
-            //Loop counts array
-            int m = 0; //Track where we are in og array
-    
-                counts[num] += 1;
-            }
-    
-            {
+    public partial class Solution {
+        public void SortColors(int[] nums) 
+        {
             int[] counts = {0,0,0};
-    
+
             //Find frequency
             foreach(var num in nums)
+            {
+                counts[num] += 1;
+            }
+
+
+            int m = 0; //Track where we are in og array
+
+            //Loop counts array
+            for(int j = 0; j < counts.Length; j++)
+            {
+                //Loop each each frequency
+                for(int n = 0; n < counts[j]; n++)
                 {
-                    nums[m] = counts[n];
+                    nums[m] = j;
                     m++;
                 }
             }
         }
     }
-        public void SortColors(int[] nums) {
-    public partial class Solution {
     public class SortColors
     {
         [Fact]
         public void SortColors_Case1()
         {
             Solution solution = new Solution();
-            Assert.Equal(X,solution.Test(x));
+            solution.SortColors([2,0,2,1,1,0]);
         }
     }
 }
