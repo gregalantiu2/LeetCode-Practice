8bf732e08563f0750e14880bbf3c47e0d5e872db
Greg Tiu <tiugreg2@gmail.com>
2024-06-29 22:44:56 -0400

    SortColors WIP

---
+++ Problems/SortColors.cs
@@ -0,0 +1,37 @@
+namespace LeetCode_Practice
+{
+                for(int n = 0; n < counts[m]; n++)
+                //Loop each each frequency
+            for(int j = 0; j < counts.Length; j++)
+            {
+    
+            //Loop counts array
+            int m = 0; //Track where we are in og array
+    
+                counts[num] += 1;
+            }
+    
+            {
+            int[] counts = {0,0,0};
+    
+            //Find frequency
+            foreach(var num in nums)
+                {
+                    nums[m] = counts[n];
+                    m++;
+                }
+            }
+        }
+    }
+        public void SortColors(int[] nums) {
+    public partial class Solution {
+    public class SortColors
+    {
+        [Fact]
+        public void SortColors_Case1()
+        {
+            Solution solution = new Solution();
+            Assert.Equal(X,solution.Test(x));
+        }
+    }
+}
