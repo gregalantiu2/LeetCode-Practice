2186358b5867febab5ecc181d5b322923939534a
Greg Tiu <tiugreg2@gmail.com>
2024-07-04 11:21:26 -0400

    Koko start

---
+++ Problems/KokoEatingBananas.cs
@@ -0,0 +1,17 @@
+namespace LeetCode_Practice
+{
+    public partial class Solution {
+        public int MinEatingSpeed(int[] piles, int h) {
+            
+        }
+    }
+    public class KokoEatingBananas
+    {
+        [Fact]
+        public void KokoEatingBananas_Case1()
+        {
+            Solution solution = new Solution();
+            Assert.Equal(X,solution.Test(x));
+        }
+    }
+}
