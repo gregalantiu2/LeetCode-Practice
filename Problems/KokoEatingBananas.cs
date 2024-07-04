18554bae0787fd1e9eed03396e11945b2b532a94
Greg Tiu <tiugreg2@gmail.com>
2024-07-04 16:55:02 -0400

    Koko brute force

--- Problems/KokoEatingBananas.cs
+++ Problems/KokoEatingBananas.cs
@@ -1,8 +1,37 @@
 namespace LeetCode_Practice
 {
-    public partial class Solution {
-        public int MinEatingSpeed(int[] piles, int h) {
-            
+    public partial class Solution
+    {
+        public int MinEatingSpeed(int[] piles, int h)
+        {
+            for (int i = 1; i <= piles.Max(); i++)
+            {
+                int hoursToEatPile = 0;
+                foreach (var pile in piles)
+                {
+                    if (pile % i > 0)
+                    {
+                        hoursToEatPile += pile / i + 1;
+                    }
+                    else
+                    {
+                        hoursToEatPile += pile / i;
+                    }
+
+
+                    if (hoursToEatPile > h)
+                    {
+                        break;
+                    }
+                }
+
+                if (hoursToEatPile <= h)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     }
     public class KokoEatingBananas
@@ -11,7 +40,7 @@ namespace LeetCode_Practice
         public void KokoEatingBananas_Case1()
         {
             Solution solution = new Solution();
-            Assert.Equal(X,solution.Test(x));
+            Assert.Equal(X, solution.Test(x));
         }
     }
 }
