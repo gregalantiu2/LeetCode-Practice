0cb83d1b16bd5f764c66a3b71b80785529f04251
Greg Tiu <tiugreg2@gmail.com>
2024-07-15 18:20:49 -0400

    ConvertBinary solved

---
+++ Problems/ConvertBinaryNumberinaLinkedListtoInteger.cs
@@ -0,0 +1,36 @@
+namespace LeetCode_Practice
+{
+    public partial class Solution {
+
+        public int GetDecimalValue(ListNode<int> head) {
+            int count = -1;
+            int result = 0;
+    
+            Queue<int> set = new Queue<int>();
+    
+            while (head != null)
+            {
+                set.Enqueue(head.val);
+                count++;
+                head = head.next;
+            }
+    
+            while (set.Any())
+            {
+                result += set.Dequeue() * (int)Math.Pow(2, count);
+                count--;
+            }
+    
+            return result;
+        }
+    }
+    public class ConvertBinaryNumberinaLinkedListtoInteger
+    {
+        [Fact]
+        public void ConvertBinaryNumberinaLinkedListtoInteger_Case1()
+        {
+            Solution solution = new Solution();
+            Assert.Equal(5, solution.(head: [1,0,1]));
+        }
+    }
+}
