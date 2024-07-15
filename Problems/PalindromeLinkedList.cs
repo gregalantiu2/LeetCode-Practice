b2fa04494d864a3a58ceca20670fcdaa828f3ae3
Greg Tiu <tiugreg2@gmail.com>
2024-07-15 15:57:39 -0400

    PalindromeLinkedList solved

---
+++ Problems/PalindromeLinkedList.cs
@@ -0,0 +1,36 @@
+namespace LeetCode_Practice
+{
+    public partial class Solution {
+        public bool IsPalindrome(ListNode head) {
+            Stack<int> set1 = new Stack<int>();
+            Queue<int> set2 = new Queue<int>();
+     
+            ListNode node = head;
+    
+            while (head != null)
+            {
+                set1.Push(head.val);
+                set2.Enqueue(head.val);
+                head = head.next;
+            }
+    
+            while (set1.Any())
+            {
+                if (set1.Pop() != set2.Dequeue())
+                    return false;
+            }
+    
+            return true;
+    
+        }
+    }
+    public class PalindromeLinkedList
+    {
+        [Fact]
+        public void PalindromeLinkedList_Case1()
+        {
+            Solution solution = new Solution();
+            //Assert.Equal(X,solution.Test(x));
+        }
+    }
+}
