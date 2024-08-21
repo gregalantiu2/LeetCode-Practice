ee934f68e9886b25edb5855c66100a5a71fc929c
Greg Tiu <tiugreg2@gmail.com>
2024-08-20 23:18:50 -0400

    Added test case

--- Problems/IncreasingOrderSearchTree.cs
+++ Problems/IncreasingOrderSearchTree.cs
@@ -1,7 +1,7 @@
 namespace LeetCode_Practice
 {
     public partial class Solution {
-        public TreeNode IncreasingBST(TreeNode root) {
+        public TreeNode<int> IncreasingBST(TreeNode<int> root) {
             if (root is null)
             {
                 return null;
@@ -20,7 +20,50 @@ namespace LeetCode_Practice
         public void IncreasingOrderSearchTree_Case1()
         {
             Solution solution = new Solution();
-            Assert.Equal(X,solution.Test(x));
+            TreeNode<int> inputRoot = new TreeNode<int>(5)
+            {
+                left = new TreeNode<int>(3)
+                {
+                    left = new TreeNode<int>(2),
+                    right = new TreeNode<int>(4)
+                },
+                right = new TreeNode<int>(6)
+                {
+                    right = new TreeNode<int>(8)
+                    {
+                        left = new TreeNode<int>(7),
+                        right = new TreeNode<int>(9)
+                    }
+                }
+            };
+            
+            TreeNode<int> expectedRoot = new TreeNode<int>(1)
+            {
+                right = new TreeNode<int>(2)
+                {
+                    right = new TreeNode<int>(3)
+                    {
+                        right = new TreeNode<int>(4)
+                        {
+                            right = new TreeNode<int>(5)
+                            {
+                                right = new TreeNode<int>(6)
+                                {
+                                    right = new TreeNode<int>(7)
+                                    {
+                                        right = new TreeNode<int>(8)
+                                        {
+                                            right = new TreeNode<int>(9)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            
+            Assert.Equal(expectedRoot, solution.IncreasingBST(inputRoot));
         }
     }
 }
